b327d9f83b5776b8dea10c1757a6b6359b99d089
marlon <marlonrocha30@gmail.com>
2020-12-30 11:25:59 -0300

    estilo customControl

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-import { SlideNav} from './slide.js'
+import SlideNav from './slide.js'
 
 
 const slide = new SlideNav('.slide', '.wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-
-slide.addControl();
+slide.addControl('.custom-control');
 
 // slide.changeSlide(3);
 // slide.activePrevSlide();
